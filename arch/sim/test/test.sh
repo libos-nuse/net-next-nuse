9de73c782f08eb278ed7826e881f94cb89fef3fc
Hajime Tazaki <tazaki@nict.go.jp>
2013-03-14 00:45:29 +0900

    update tests with bake repository, tests

--- arch/sim/test/test.sh
+++ arch/sim/test/test.sh
@@ -16,11 +16,4 @@ fi
 export NS_ATTRIBUTE_DEFAULT='ns3::DceManagerHelper::LoaderFactory=ns3::DlmLoaderFactory[];ns3::TaskManager::FiberManagerType=UcontextFiberManager' 
 
 cd buildtop/source/dce
-LD_LIBRARY_PATH=${srctree} ./test.py -z ns3test-dce-vdl -n ${VALGRIND} ${FAULT_INJECTION}
-cd ..
-cd dce-quagga
-LD_LIBRARY_PATH=${srctree} ../dce/test.py -n -z ns3test-dce-quagga-vdl ${VALGRIND} ${FAULT_INJECTION}
-cd ..
-cd dce-umip
-LD_LIBRARY_PATH=${srctree} ../dce/test.py -n -z ns3test-dce-umip-vdl ${VALGRIND} ${FAULT_INJECTION}
-cd ..
+LD_LIBRARY_PATH=${srctree} ./test.py -z test-runner-vdl -n ${VALGRIND} ${FAULT_INJECTION}
