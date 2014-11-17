7208e2e8d15f7db62db38c44efe924306b09eb8a
Hajime Tazaki <thehajime@gmail.com>
2014-11-17 09:58:38 +0900

    Add more debug message

--- arch/sim/test/nuse/nuse-test.sh
+++ arch/sim/test/nuse/nuse-test.sh
@@ -4,4 +4,9 @@ IFNAME=`ip route |grep default | awk '{print $5}'`
 GW=`ip route |grep default | awk '{print $3}'`
 #XXX
 IPADDR=`echo $GW | sed -r "s/([0-9]+\.[0-9]+\.[0-9]+\.)([0-9]+)$/\1\`expr \2 + 10\`/"`
+
+ip route
+ip address
+ip link
+
 sudo NUSEDEV=${IFNAME} nuse-${IFNAME}=${IPADDR} ./nuse ping ${GW} -c 1
