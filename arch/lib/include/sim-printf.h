54c539d7fec18cf2cc424035232b2b3fd91819ae
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2015-04-26 07:08:17 +0900

    lib: public headers and API implementations for userspace programs
    userspace programs which uses libos access via a public API, lib_init(),
    with passed arguments struct SimImported and struct SimExported.
    
    Signed-off-by: Hajime Tazaki <tazaki@sfc.wide.ad.jp>
    Signed-off-by: Ryo Nakamura <upa@haeena.net>

---
+++ arch/lib/include/sim-printf.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 INRIA, Hajime Tazaki
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@gmail.com>
+ *         Hajime Tazaki <tazaki@sfc.wide.ad.jp>
+ */
+
+#ifndef SIM_PRINTF_H
+#define SIM_PRINTF_H
+
+void lib_printf(const char *str, ...);
+
+#endif /* SIM_PRINTF_H */
