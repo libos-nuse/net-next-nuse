libos-nuse/net-next-nuse

commit 0e4513f86ad098542ea9b0ae1cc9972554c6a253

lib: add atomic_long_ops into libos asm code
added atomic64_or(), atomic64_xor(), atomic64_andnot() for newly added
ops.

Fixes: 90fe65148ea7 ("atomic: Add atomic_long_t bitops")
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

arch/lib/include/asm/atomic.h
@@ -44,6 +44,21 @@ static void atomic64_and(long long i, atomic64_t *v)
 	v->counter &= i;
 }
 
+static void atomic64_or(long i, atomic64_t *v)
+{
+	v->counter |= i;
+}
+
+static void atomic64_xor(long i, atomic64_t *v)
+{
+	v->counter ^= i;
+}
+
+static void atomic64_andnot(long i, atomic64_t *v)
+{
+	atomic64_and(~i, v);
+}
+
 long atomic64_sub_return(long i, atomic64_t *v);
 #define atomic64_inc_return(v)  (atomic64_add_return(1, (v)))
 #define atomic64_dec_return(v)  (atomic64_sub_return(1, (v)))

commit 8d76510b64ea910ff061fa0f71232019d52ff539

lib: remove atomic64_andnot() because of conflicts
atomic64_andnot() is defined in include/linux/atomic.h so no need to
define in arch-specific code.

Fixes: 0e4513f86ad0 ("lib: add atomic_long_ops into libos asm code")
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

arch/lib/include/asm/atomic.h
@@ -54,11 +54,6 @@ static void atomic64_xor(long i, atomic64_t *v)
 	v->counter ^= i;
 }
 
-static void atomic64_andnot(long i, atomic64_t *v)
-{
-	atomic64_and(~i, v);
-}
-
 long atomic64_sub_return(long i, atomic64_t *v);
 #define atomic64_inc_return(v)  (atomic64_add_return(1, (v)))
 #define atomic64_dec_return(v)  (atomic64_sub_return(1, (v)))